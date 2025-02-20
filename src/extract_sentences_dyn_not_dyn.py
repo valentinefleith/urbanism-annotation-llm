36510e651ed498749c239437d48a63940fa34f48
valentinefleith <valentine.fleith@gmail.com>
2025-02-20 18:37:12 +0100

    add results mistral

--- src/extract_sentences_dyn_not_dyn.py
+++ src/extract_sentences_dyn_not_dyn.py
@@ -1,25 +1,25 @@
-import os 
+import os
 import xml.etree.ElementTree as ET
 import pandas as pd
 
 
-def save_to_csv(data, output_file) : 
+def save_to_csv(data, output_file):
     """
     Enregistre les phrases extraites du fichier XML dans un fichier CSV.
-    
+
     Args:
         data (tuples): Liste contenant (phrase, 0 (s'il n'y a pas de dynamique) ou 1 (s'il y en a une)).
         output_file (str): Chemin du fichier de sortie.
     """
-    if data: 
+    if data:
         df = pd.DataFrame(data, columns=["sentence", "dynamic"])
         df.to_csv(output_file, sep="|")
         print(f"Le fichier .csv: {output_file} a été enregistré.")
 
 
-def process_dynamic_xmlfile(dossier_input, dossier_output) :
+def process_dynamic_xmlfile(dossier_input, dossier_output):
     """
-    Parcourt tous les fichiers XML d'un dossier, extrait les phrases. 
+    Parcourt tous les fichiers XML d'un dossier, extrait les phrases.
     Création d'un tuple (phrase, 0 (s'il n'y a pas de dynamique) ou 1 (s'il y en a une)).
     Enregistre en CSV.
 
@@ -42,19 +42,14 @@ def process_dynamic_xmlfile(dossier_input, dossier_output) :
                 data.append((no_tags_text, 1))
             else:
                 mots = no_tags_text.split()
-                if len(mots) > 10:      #on définit qu'une phrase c'est 10 mots
+                if len(mots) > 10:  # on définit qu'une phrase c'est 10 mots
                     data.append((no_tags_text, 0))
-
 
         output_file = os.path.join(dossier_output, fichier_xml.replace(".xml", ".csv"))
         save_to_csv(data, output_file)
 
 
-#Lancement
+# Lancement
 dossier_input = "../corpus/corpus_annote"
 dossier_output = "../corpus/corpus_phrases"
 process_dynamic_xmlfile(dossier_input, dossier_output)
-
-
-
-
