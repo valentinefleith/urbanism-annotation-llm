56524936f325a4ac6bda1494db2ad80fa6044d39
maouee <maoui.gourves@gmail.com>
2025-05-22 23:46:13 +0200

    ajout prompt multiclass

--- src/categories.py
+++ src/categories.py
@@ -4,6 +4,6 @@ from enum import Enum
 class Categories(Enum):
     Creation = "crea"
     Destruction = "destr"
-    Modification = "modi"
-    Reprise = "rep"
-    Maintien = "main"
+    Modification = "modif"
+    Reprise = "repr"
+    Maintien = "maint"
